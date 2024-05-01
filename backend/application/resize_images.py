730d70bf013c0c943d94b0cbb0ad1d355e7e49fd
dedWalker18 <84268678+dedWalker18@users.noreply.github.com>
2024-05-02 00:42:26 +0530

    Update resize_images.py

--- backend/application/resize_images.py
+++ backend/application/resize_images.py
@@ -1,11 +1,9 @@
 import os
 from PIL import Image
     
-def resize_image(image):            ## This is actually expecting the file path to that "image"
+def resize_image(image):                            ## path to image expected!!
     try:
         img_object = Image.open(image)
-        print(f"Image opened: {image}")
-        print(img_object.size)
         newsize = [400,400]
         resized_img = img_object.resize(newsize)
         resized_img.save(image)
